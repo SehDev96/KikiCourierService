e2f57b617bc5cd1ffc8a69fa9cc998ef8ee41278
Sehran <seh96.dev@gmail.com>
2023-04-08 21:41:59 +0800

    added loop to prompt user input if error is input format related exception

--- src/main/java/com/kiki/KikiApplication.java
+++ src/main/java/com/kiki/KikiApplication.java
@@ -11,36 +11,42 @@ import java.util.Scanner;
 
 public class KikiApplication {
     public static void main(String[] args) {
-        try {
-            String inputString = getUserInput().toString();
-            DeliveryRequest deliveryRequest = DeliveryRequest.getInstance().parseDeliveryRequestFromString(inputString);
-            DeliveryService deliveryService = new DeliveryService();
-            deliveryService.calculateDeliveryCost(deliveryRequest);
-            if (deliveryRequest.getNumberOfVehicles() > 0) {
-                ShipmentManager shipmentManager = new ShipmentManager();
-                List<Shipment> shipmentList = shipmentManager.sortPackagesIntoShipments(deliveryRequest.getPackageDetailsList());
-                deliveryService.calculateDeliveryTime(shipmentList);
-            }
-            System.out.println(DeliverySummary.getInstance().toString());
-        } catch (Exception e){
-            System.err.println("Error: " + e.getMessage());
-            if(e instanceof IllegalArgumentException){
-                System.out.println("Please input the details according to standard format.");
-                System.out.println("Standard format: ");
-                System.out.println("------------------------------------------------");
-                System.out.println("base_delivery_cost no_of_packges\n" +
-                        "pkg_id1 pkg_weight1_in_kg distance1_in_km offer_code1\n" +
-                        " ....");
-                System.out.println("---------------------OR-------------------------");
-                System.out.println("base_delivery_cost no_of_packges\n" +
-                        "pkg_id1 pkg_weight1_in_kg distance1_in_km offer_code1\n" +
-                        " ....\n" +
-                        "no_of_vehicles max_speed max_carriable_weight");
-                System.out.println("------------------------------------------------");
-
-
+        while (true) {
+            try {
+                String inputString = getUserInput().toString();
+                DeliveryRequest deliveryRequest = DeliveryRequest.getInstance().parseDeliveryRequestFromString(inputString);
+                DeliveryService deliveryService = new DeliveryService();
+                deliveryService.calculateDeliveryCost(deliveryRequest);
+                if (deliveryRequest.getNumberOfVehicles() > 0) {
+                    ShipmentManager shipmentManager = new ShipmentManager();
+                    List<Shipment> shipmentList = shipmentManager.sortPackagesIntoShipments(deliveryRequest.getPackageDetailsList());
+                    deliveryService.calculateDeliveryTime(shipmentList);
+                }
+                System.out.println(DeliverySummary.getInstance().toString());
+                break; // Exit the loop if everything was successful
+            } catch (Exception e){
+                System.err.println("Error: " + e.getMessage());
+                if(e instanceof IllegalArgumentException){
+                    System.out.println("Please input the details according to standard format.");
+                    System.out.println("Standard format: ");
+                    System.out.println("------------------------------------------------");
+                    System.out.println("base_delivery_cost no_of_packges\n" +
+                            "pkg_id1 pkg_weight1_in_kg distance1_in_km offer_code1\n" +
+                            " ....");
+                    System.out.println("---------------------OR-------------------------");
+                    System.out.println("base_delivery_cost no_of_packges\n" +
+                            "pkg_id1 pkg_weight1_in_kg distance1_in_km offer_code1\n" +
+                            " ....\n" +
+                            "no_of_vehicles max_speed max_carriable_weight");
+                    System.out.println("------------------------------------------------");
+                } else {
+                    // break the loop if Exception is other than input related exception.
+                    break;
+                }
+                System.out.println("Please enter the input again:");
             }
         }
+
     }
 
     private static StringBuilder getUserInput() {
